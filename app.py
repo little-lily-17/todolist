3bd324981c3a7ba019f051625d9298e52d1aa431
Nurlaili Fajriyah <nurlaili.fajriyah@gmail.com>
2019-12-31 09:50:18 +0700

    Adjust create() method to be able to insert to mongodb (#5)
    * Adjust create() method to be able to insert to mongodb
    
    * Adjust create() method to be able to insert to mongodb
    
    * Add new dependency (pymongo) to requirements file
    
    Co-authored-by: Izzati Alvandiar <raidnav@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['to-do-list']
+collection = db['to-do-list']
 
 items = []
 
@@ -9,23 +14,29 @@ items = []
 def hello_world():
     return 'Welcome, this is my todo list!'
 
+
 @app.route('/todo', methods=['POST'])
 def create():
-    items.append(request.json['data'])
-    return jsonify('items', items)
+    items.append(request.json['data']) #will be deleted later
+    req_data = request.get_json()
+    collection.insert_one(req_data).inserted_id
+    return "204"
 
 @app.route('/todo', methods=['GET'])
 def getall():
     return jsonify('items', items)
 
+
 @app.route('/todo/<int:index>', methods=['GET'])
 def get(index):
     return items[index]
+
 
 @app.route('/todo/<int:index>/<string:word>', methods=['PUT'])
 def update(index, word):
     items[index] = word
     return jsonify('items', items)
+
 
 @app.route('/todo/<int:index>', methods=['DELETE'])
 def delete(index):
