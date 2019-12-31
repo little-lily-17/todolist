25ba74fd0a13adb791e9a542c78b1217bda0678c
nurlailifajriyah <nurlaili.fajriyah@gmail.com>
2019-12-31 20:39:09 +0700

    Connect all functions with mongodb

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, json
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
@@ -19,29 +20,40 @@ def hello_world():
 def create():
     items.append(request.json['data']) #will be deleted later
     req_data = request.get_json()
-    collection.insert_one(req_data).inserted_id
-    return "204"
+    req_data['seq'] = collection.count()
+    collection.insert_one(req_data)
+    return "inserted"
 
 @app.route('/todo', methods=['GET'])
 def getall():
-    return jsonify('items', items)
-
+    results = []
+    for x in collection.find():
+        results.append(x)
+    return dumps(results)
 
 @app.route('/todo/<int:index>', methods=['GET'])
 def get(index):
-    return items[index]
+    return dumps(collection.find_one({"seq": index}))
 
 
 @app.route('/todo/<int:index>/<string:word>', methods=['PUT'])
 def update(index, word):
-    items[index] = word
-    return jsonify('items', items)
+    collection.find_one_and_update({"seq": index},
+                                 {"$set": {"data": word}})
+    return dumps(collection.find_one({"seq": index}))
 
 
 @app.route('/todo/<int:index>', methods=['DELETE'])
 def delete(index):
-    items.pop(index)
-    return jsonify('items', items)
+    collection.delete_one({"seq": index})
+    return "deleted"
 
 if __name__ == '__main__':
     app.run()
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
